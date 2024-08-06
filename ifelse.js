28f71707f6ba1577d19f3411f341e042977738a4
cyalater00 <Jachimikeonwuka@gmail.comgit config --global user.name  JACHIMIKE ONWUKAgit config --global user.email Jachimikeonwuka@gmail.com>
2024-08-06 13:47:55 -0400

    Alab assignment

---
+++ ifelse.js
@@ -0,0 +1,24 @@
+// let userAge = 17;
+
+// if (userAge >= 18) {
+//     console.log('You shall pass');
+// } else{
+//     console.log('you shall not pass.');
+// }
+
+// ==== Grade Exercise ===
+yourGrade(61);
+function yourGrade(score) {
+  let grade;
+  if (score >= 90) {
+    console.log("your grade is A");
+  } else if (score >= 80) {
+    console.log("your grade is B");
+  } else if (score >= 70) {
+    console.log("your grade C");
+  } else if (score >= 60) {
+    console.log("your grade is D");
+  } else {
+    console.log("your grade is F");
+  }
+}
